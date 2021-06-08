ccad33ebbe0b8661bcc997cff46fa9f9839b0ebe
Aswin Lal M <aswnss.m@gmail.com>
2021-06-08 22:05:09 +0530

    Working on form and db

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -9,6 +9,7 @@ db = SQLAlchemy(app)
 class Events(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     event_name = db.Column(db.String(100), nullable=False,primary_key=True)
+    event_details = db.Column(db.String(500), nullable=False,nullable=False)
     organiser = db.Column(db.String(32), nullable=False)
     Start_date = db.Column(db.DateTime, nullable=False, default= datetime.utcnow)
     End_date = db.Column(db.DateTime, nullable=False)
@@ -25,8 +26,18 @@ images = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q']
 def home():
     return render_template('index.html',images=images)
 
-@app.route('/forms')
+@app.route('/forms', methods=['GET','POST'])
 def form():
+    if request.method=='POST':
+        evtTitle= request.form['eventName']
+        evtOrg = request.form['organizer']
+        evtPstr = request.form['eventPoster']
+        evtDtls = request.form['eventDetails']
+        evntStrDt = request.form['startDate']
+        evntEndDt = request.form['endDate']
+        evtLnk = request.form['regLink']
+        evtCtc =[request.form['contactOne'],request.form['contactTwo']] 
+        
     return render_template('form.html')
  
 if __name__ == "__main__":
