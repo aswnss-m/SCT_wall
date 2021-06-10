b608a0bff1459d08233d5ae3ba7b807b96537ee5
Aswin Lal M <aswnss.m@gmail.com>
2021-06-10 16:00:55 +0530

    fixed database

--- app/app.py
+++ app/app.py
@@ -1,24 +1,26 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Events.db'
 db = SQLAlchemy(app)
 
 class Events(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    event_name = db.Column(db.String(100), nullable=False,primary_key=True)
+    id = db.Column(db.Integer,db.Sequence('seq_reg_id', start=1, increment=1),primary_key=True)
+    event_name = db.Column(db.String(100), nullable=False)
     event_details = db.Column(db.String(500),nullable=False)
     organiser = db.Column(db.String(32), nullable=False)
-    Start_date = db.Column(db.DateTime, nullable=False, default= datetime.utcnow)
-    End_date = db.Column(db.DateTime, nullable=False)
+    start_date = db.Column(db.String(12), nullable=False, default= str(datetime.utcnow()))
+    date_created = db.Column(db.DateTime,nullable=False,default=datetime.utcnow())
+    end_date = db.Column(db.String(12), nullable=False)
     Link = db.Column(db.String(100))
     contact1 = db.Column(db.String(10))
     contact2 = db.Column(db.String(10))
 
-    def __repr__():
-        return self.organiser +" "+ str(self.id)
+    def __repr__(self):
+        data = self.organiser +" "+ str(self.id)
+        return data
 
 images = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q']
 @app.route('/')
@@ -31,19 +33,24 @@ def form():
     if request.method=='POST':
         evtTitle= request.form['eventName']
         evtOrg = request.form['organizer']
-        evtPstr = request.form['eventPoster']
+        # evtPstr = request.form['eventPoster']
         evtDtls = request.form['eventDetails']
         evntStrDt = request.form['startDate']
         evntEndDt = request.form['endDate']
         evtLnk = request.form['regLink']
         evtCtc =[request.form['contactOne'],request.form['contactTwo']] 
         
-        new_event = Events(event_name=evtTitle,event_details=evtDtls,organiser=evtOrg,Start_date=evntStrDt,End_date=evntEndDt,Link=evtLnk,contact1=evtCtc[0],contact2=evtCtc[1])
+        new_event = Events(event_name=evtTitle,event_details=evtDtls,organiser=evtOrg,start_date=evntStrDt,end_date=evntEndDt,Link=evtLnk,contact1=evtCtc[0],contact2=evtCtc[1])
 
         db.session.add(new_event)
         db.session.commit()
+
+        return redirect('/home')
+    else:
+        pass
         
     return render_template('form.html')
- 
+
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
