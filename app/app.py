ebb97b034ed9aac6e3e929c711865c2e607d1218
Aswin Lal M <aswnss.m@gmail.com>
2021-06-10 16:17:08 +0530

    implementing image upload

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Events.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 class Events(db.Model):
@@ -17,6 +20,10 @@ class Events(db.Model):
     Link = db.Column(db.String(100))
     contact1 = db.Column(db.String(10))
     contact2 = db.Column(db.String(10))
+    
+    poster = db.Column(db.Text, unique=True, nullable=False)
+    poster_name = db.Column(db.Text, nullable=False)
+    poster_mimetype = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         data = self.organiser +" "+ str(self.id)
@@ -33,14 +40,17 @@ def form():
     if request.method=='POST':
         evtTitle= request.form['eventName']
         evtOrg = request.form['organizer']
-        # evtPstr = request.form['eventPoster']
+        evtPstr = request.files['eventPoster']
         evtDtls = request.form['eventDetails']
         evntStrDt = request.form['startDate']
         evntEndDt = request.form['endDate']
         evtLnk = request.form['regLink']
         evtCtc =[request.form['contactOne'],request.form['contactTwo']] 
         
-        new_event = Events(event_name=evtTitle,event_details=evtDtls,organiser=evtOrg,start_date=evntStrDt,end_date=evntEndDt,Link=evtLnk,contact1=evtCtc[0],contact2=evtCtc[1])
+        filename = secure_filename(evtPstr.filename)
+        mimetype = evtPstr.mimetype
+
+        new_event = Events(event_name=evtTitle,event_details=evtDtls,organiser=evtOrg,start_date=evntStrDt,end_date=evntEndDt,Link=evtLnk,contact1=evtCtc[0],contact2=evtCtc[1],poster=evtPstr.read(),poster_name=filename,poster_mimetype=mimetype)
 
         db.session.add(new_event)
         db.session.commit()
